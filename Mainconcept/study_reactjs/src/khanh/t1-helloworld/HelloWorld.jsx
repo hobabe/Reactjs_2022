c58d8595e254cee9a488c08dd03cb9364895019e
Xuan Khanh <xuankhanh379@gmail.com>
2022-10-29 23:16:44 +0700

    change tag h3 to h1 component HelloWorld

--- Mainconcept/study_reactjs/src/khanh/t1-helloworld/HelloWorld.jsx
+++ Mainconcept/study_reactjs/src/khanh/t1-helloworld/HelloWorld.jsx
@@ -1,7 +1,7 @@
 export default function HelloWorld() {
     return (
         <div className="style">
-            <h3>Unit 1 "hello world"</h3>
+            <h1>Unit 1 "hello world"</h1>
             <p>
                 const root = ReactDOM.createRoot(document.getElementById('root'));
                 root.render(&lt; h1 &gt; Hello, world!&lt;/h1&gt;);
