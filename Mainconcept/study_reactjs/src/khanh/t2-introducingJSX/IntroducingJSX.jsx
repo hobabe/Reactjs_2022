ca7a0d8a93e241f9cacf024251afcb4c18f0e89a
Xuan Khanh <xuankhanh379@gmail.com>
2022-11-04 23:07:45 +0700

    repair and update content Unit 2: Introducing JSX

--- Mainconcept/study_reactjs/src/khanh/t2-introducingJSX/IntroducingJSX.jsx
+++ Mainconcept/study_reactjs/src/khanh/t2-introducingJSX/IntroducingJSX.jsx
@@ -4,18 +4,22 @@ export default function IntroducingJSX() {
         <div className="style">
             <h1>Unit 2:  {unit}
             </h1>
-            <p className="content">
-                <h3>Consider this variable declaration:</h3>
+            <div className="content">
+
+                <label>JSX là cú pháp mở rộng đến từ JavaScipt, JSX trông giống ngôn ngữ mẫu, nhưng JSX đi kèm toàn bộ tính năng của JavaScript.<br />
+                    JSX làm ra những React "element."
+                </label>
                 const element = &lt;h1&gt;Hello, world!&lt;/h1&gt;;
-            </p>
-            <p className="content">
+            </div><br />
+            <div className="content">
                 <h3>Embedding Expressions in JSX</h3>
+                <label>Có thể nhúng bất kỳ biểu thức JavaScript hợp lệ bên trong JSX bằng cặp dấu ngoặc nhọn.</label><br />
                 const name = 'Josh Perez';
                 const element =  &lt;h1&gt;Hello, {"{name}"} &lt;h1&gt;;
-            </p>
-            <p className="content">
+            </div><br />
+            <div className="content">
                 <h3>JSX is an Expression Too</h3>
-
+                <label> Can use JSX inside of if statements and for loops</label><br />
 
                 <label> function getGreeting(user) {"{"}</label><br />
                 <label> if (user)  {"{"}</label><br />
@@ -24,15 +28,15 @@ export default function IntroducingJSX() {
                 <label>  return &lt;h1&gt;Hello, Stranger.&lt;/h1&gt;;</label><br />
                 <label>  {"}"}</label>
 
-            </p>
-            <p className="content">
+            </div><br />
+            <div className="content">
                 <h3>Specifying Attributes with JSX</h3>
                 <p>You may use quotes to specify string literals as attributes:</p>
                 const element = &lt;a href="https://www.reactjs.org"&gt; link &lt;/a&gt;;
                 <p>You may also use curly braces to embed a JavaScript expression in an attribute:</p>
                 const element = &lt;img src={"{user.avatarUrl}"}&gt;&lt;/img&gt;
 
-            </p>
+            </div>
             <p className="content">
                 <h3>Specifying Children with JSX</h3>
                 <p>If a tag is empty, you may close it immediately with /&gt;, like XML:</p>
